73d11d0d349c217313520ccfcd0ffc3d6e7aac7a
Federico Juan <99974552+FedeJuan@users.noreply.github.com>
2022-03-08 12:22:37 -0300

    Update Particiones.hs

--- Particiones/Particiones.hs
+++ Particiones/Particiones.hs
@@ -1,51 +1,31 @@
--- funcion que recibe un numero y devuelve la cantidad de particiones que tiene un numero.
+-- funcion que cuenta la cantidad de particiones que tiene un numero
 respuesta :: Int -> Int
-respuesta n  | n < 2 = 0
-             | otherwise = length(conjuntoreal(listasumas n))
+respuesta n = length(particiones n)
 
--- funcion que recibe un set de conjuntos de numeros y elimina los conjuntos repetidos, dejando el que corresponde.
-conjuntoreal :: [[Int]] -> [[Int]]
-conjuntoreal [] = []
-conjuntoreal (x:xs) | verifica x xs = conjuntoreal xs
-                    | otherwise = x:(conjuntoreal xs)
+-- funcion que muestra todas las particiones que tiene un numero.
+particiones :: Int -> [[Int]]
+particiones 1 = [[1]]
+particiones n = [n]:(conjuntos 1 (n-1) 0 n)
 
--- funcion que recibe un conjunto y un set, y devuelve True si ese conjunto está dentro del set, o False si no está.
-verifica :: [Int] -> [[Int]] -> Bool
-verifica x [] = False
-verifica x xs | x == (head xs) = True
-              | otherwise = verifica x (tail xs)
+-- funcion que toma un numero n y devuelve un conjunto formado por el resultado de meter el numero m en las particiones de
+-- n-1 , y luego, utilizando la recursión, va metiendo distintos m en particiones de n que no contengan numeros mas chicos
+-- que m.
+conjuntos :: Int -> Int -> Int -> Int -> [[Int]]
+conjuntos m 0 o c = []
+conjuntos m n 0 c = metecjto (meter m (particiones n)) (conjuntos (m+1) (n-1) 1 c)
+conjuntos m n o c | m > (div c 2) = []
+                  | otherwise = metecjto (meter m (conjuntosin (particiones n) o m)) (conjuntos (m+1) (n-1) (o+1) c)
 
--- funcion que recibe un numero y devuelve un set que contiene conjuntos de numeros que si sumas entre ellos, el 
--- resultado da el numero ingresado por el usuario.
-listasumas :: Int -> [[Int]]
-listasumas 2 = [[1,1]]
-listasumas n = metecjto (irmetiendo 1 (n-1)) (sumas (n-1) 1 n)
+conjuntosin :: [[Int]] -> Int -> Int -> [[Int]]
+conjuntosin [] o c = []
+conjuntosin (x:xs) o c | o == c = x:(conjuntosin xs 1 c)
+                       | elem o x = conjuntosin xs 1 c
+                       | otherwise = conjuntosin (x:xs) (o+1) c
 
--- funcion que recibe dos sets de conjuntos y devuelve un set con todos los conjuntos de ambos sets.
-metecjto :: [[Int]] -> [[Int]] -> [[Int]]
-metecjto [] b = b
-metecjto a b = metecjto (tail a) ((head a):b)
-
-irmetiendo :: Int -> Int -> [[Int]]
-irmetiendo a b | b == 1 = []
-               | otherwise = (metecjto (ccordenado(meter a (listasumas b))) (irmetiendo (a+1) (b-1)))
-
-conjuntoordenado :: [Int] -> [Int]
-conjuntoordenado [] = []
-conjuntoordenado (x:xs) | xs == [] = [x]
-                        | x > head xs = (head xs):(conjuntoordenado (x:(tail xs)))
-                        | otherwise = (x:xs)
-                        
-ccordenado :: [[Int]] -> [[Int]]
-ccordenado [] = []
-ccordenado (x:xs) = (conjuntoordenado x):(ccordenado xs)
-
-sumas :: Int -> Int -> Int -> [[Int]]
-sumas a b n | (a < n `quot` 2) || ([a,b] == [(b-1),(a+1)]) = []
-            | otherwise = (conjuntoordenado [a,b]):(sumas (a-1) (b+1) n)
-
--- funcion que recibe un numero y un set de conjuntos, y devuelve ese set con el elemento adentro de cada
--- conjunto perteneciente al set.
 meter :: Int -> [[Int]] -> [[Int]]
 meter a [] = []
 meter a (x:xs) = (a:x):(meter a xs)
+
+metecjto :: [[Int]] -> [[Int]] -> [[Int]]
+metecjto [] b = b
+metecjto a b = metecjto (tail a) ((head a):b)
